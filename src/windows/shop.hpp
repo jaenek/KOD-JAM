0c02d20595407c43943510b9cd0e369a836ac7bb
jaenek <jaenek@protonmail.com>
2020-12-30 00:43:14 +0100

    Separate headers

--- src/windows/shop.hpp
+++ src/windows/shop.hpp
@@ -3,14 +3,9 @@
 #include <raylib.h>
 
 #include "window.hpp"
-#include "main_window.hpp"
-
 #include "../buttons/navigate_buttons.hpp"
 #include "../buttons/shop_buttons.hpp"
-
 #include "../dwarf/dwarf.hpp"
-
-#include "mine.hpp"
 
 class shop : window
 {
@@ -23,35 +18,9 @@ private:
 	dwarf& player; //referencja do ekwipunku gracza
 
 public:
-	shop(dwarf& player):
-		leave_shop(425, 250, 150, 75, "Back to town", 20, GRAY, LIGHTGRAY, BROWN, GRAY, DARKBROWN, GRAY),
-		buy_food(50, 450, 150, 75, "Buy food", 20, GRAY, LIGHTGRAY, BROWN, GRAY, DARKBROWN, GRAY, &player),
-		buy_torch(425, 450, 150, 75, "Buy torch", 20, GRAY, LIGHTGRAY, BROWN, GRAY, DARKBROWN, GRAY, &player),
-		upgrade_pickaxe(850, 450, 200, 75, "Upgrade pickaxe", 20, GRAY, LIGHTGRAY, BROWN, GRAY, DARKBROWN, GRAY, &player),
-		
-		player(player){}
+	shop(dwarf& player);
 
-	void update()
-	{
-		leave_shop.update();
-		buy_food.update();
-		buy_torch.update();
-		upgrade_pickaxe.update();
+	void update();
 
-		buy_food.add_food();
-		buy_torch.add_torch();
-		upgrade_pickaxe.upgrade_pickaxe();
-	}
-
-	void draw()
-	{	
-		leave_shop.draw();
-		buy_food.draw();
-		buy_torch.draw();
-		upgrade_pickaxe.draw();
-
-		const char* text = "SHOP";
-		DrawText(text, (window_width - MeasureText(text, 30)) / 2, 50, 30, DARKGRAY);
-	}
+	void draw();
 };
-
